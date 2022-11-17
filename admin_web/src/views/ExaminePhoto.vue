0121951e650084e47a082d90bc2b756a13af3a91
NJNUqhx <3380595029@qq.com>
2022-11-17 20:19:39 +0800

    qhx 修改审核照片样式

--- admin_web/src/views/ExaminePhoto.vue
+++ admin_web/src/views/ExaminePhoto.vue
@@ -1,5 +1,46 @@
 <template>
-    <ContentField>
+    <div class="container">
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th style="width: 40%;"><span>待审核照片</span></th>
+                    <th style="width: 25%"><span>照片信息</span></th>
+                    <th style="width: 25%"><span>审核意见</span></th>
+                    <th style="width: 10%"><span>审核操作</span></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="photo in photos" :key="photo.id">
+                    <td>
+                        <div class="card">
+                            <div class="card-body">
+                                <img :src="photo.address" class="img-fluid img-thumbnail" alt="待审核照片">
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <ul class="list-group">
+                            <li class="list-group-item">所属用户: {{ photo.userId }}</li>
+                            <li class="list-group-item">照片编号: {{ photo.photoId }}</li>
+                            <li class="list-group-item">上传时间: {{ photo.time }}</li>
+                        </ul>
+                    </td>
+                    <td>
+                        <div class="input-group">
+                            <textarea class="form-control" placeholder="审核意见" style="height: 100px;"></textarea>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="btn-group-vertical">
+                            <button class="btn btn-success" style="margin-top: 10%">通过</button>
+                            <button class="btn btn-danger">拒绝</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- <ContentField>
         <div class="row">
             <div class="col-3">
                 <img src="http://ceai.njnu.edu.cn/UploadFiles/2022-04/2/2022042012064779273.png" class="img-fluid"
@@ -23,18 +64,45 @@
                 </div>
             </div>
         </div>
-    </ContentField>
+    </ContentField> -->
 </template>
 
 <script>
-import ContentField from '../components/ContentField'
+import { reactive } from 'vue'
 
 export default {
     name: "ExaminePhoto",
     components: {
-        ContentField
+    },
+    setup() {
+        const photos = reactive([
+            {
+                address: "http://ceai.njnu.edu.cn/UploadFiles/2022-04/2/2022042012064779273.png",
+                userId: "njnuqhx",
+                photoId: 1,
+                time: "20221117"
+            },
+            {
+                address: "http://ceai.njnu.edu.cn/UploadFiles/2021-12/2/2021121911222992737.jpg",
+                userId: "njnuxzl",
+                photoId: 2,
+                time: "20221118"
+            },
+            {
+                address: "http://ceai.njnu.edu.cn/UploadFiles/2020-12/13/2020122515095885244.jpg",
+                userId: "njnucsq",
+                photoId: 1,
+                time: "20221119"
+            }
+        ]);
+        return {
+            photos
+        }
     }
+
 }
+
+
 </script>
 
 <style scoped>
