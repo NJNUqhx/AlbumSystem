c4ca966e621a12e910c6d283144c57a74103a552
NJNUqhx <3380595029@qq.com>
2022-11-19 11:03:53 +0800

    qhx 为用户后端配置依赖

---
+++ admin_backend/src/main/java/com/albumsystem/admin_backend/pojo/Comment.java
@@ -0,0 +1,17 @@
+package com.albumsystem.admin_backend.pojo;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @TableId(type = IdType.AUTO)
+    private Integer commentId;
+
+}
