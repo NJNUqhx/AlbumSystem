90c53ce44e50ccac9373a3c021caac6696f0e35b
NJNUqhx <3380595029@qq.com>
2022-11-22 16:33:29 +0800

    qhx 创建pojo层存储管理相关的类

---
+++ user_backend/src/main/java/com/albumsystem/user_backend/pojo/Photo.java
@@ -0,0 +1,25 @@
+package com.albumsystem.user_backend.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Photo {
+    @TableId(type = IdType.AUTO)
+    private Integer photoId;
+    private Integer userId;
+    private String name;
+    private Integer authority;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date time;
+    private Integer status;
+    private String address;
+}
