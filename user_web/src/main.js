1d46de1048d0b2469fcc22da662d935646ade02a
Xuezi_Long <75626278+Godzilla0209@users.noreply.github.com>
2022-12-05 18:14:09 +0800

    add element

--- user_web/src/main.js
+++ user_web/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import installElementPlus from './plugins/element'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+installElementPlus(app)
+app.use(store).use(router).mount('#app')
